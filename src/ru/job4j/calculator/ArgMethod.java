5593f87f38e09b0adda8e254a397fd95bab80a49
Aleksey VV <avbavnn@gmail.com>
2020-09-05 21:58:34 +0300

    5. Конвертер валюты [#354567]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -1,18 +1,13 @@
 package ru.job4j.calculator;
 
 public class ArgMethod {
-
     public static void hello(String name) {
         System.out.println("Hello, " + name);
     }
-
     public static void main(String[] args) {
         String name = "Petr Arsentev";
         String age = "33";
-
         ArgMethod.hello(name);
-
         ArgMethod.hello(age);
-
     }
 }
