07deb469d119c119c7ec5b1e7cc76c56b85aa052
Aleksey VV <avbavnn@gmail.com>
2020-09-08 22:22:36 +0300

    3. Тесты для определения расстояния между точками. [#354602]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,17 +1,17 @@
 package ru.job4j.condition;
 
 public class Point {
-    public static double distance( int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    public static double distance(int x1, int y1, int x2, int y2) {
+        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
         return rsl;
     }
 
-    public static void main(String[] args){
-        double result = Point.distance(0,0,2,0);
+    public static void main(String[] args) {
+        double result = Point.distance(0 ,0 ,2 ,0);
         System.out.println("result (0,0) to (2,0) " + result);
         result = Point.distance(1,2,11,12);
         System.out.println("result (1,2) to (11,12) " + result);
-        result = Point.distance(-10,-10,40,20);
+        result = Point.distance(-10,40,- 10,20);
         System.out.println("result (-10,-10) to (40,20) " + result);
     }
 }
---
+++ test/ru/job4j/condition/PointTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.condition;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void when12to1112then14() {
+        double expected = 14.14;
+        int x1 = 1;
+        int y1 = 2;
+        int x2 = 11;
+        int y2 = 12;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+    @Test
+    public void when1010to4020then58() {
+        int expected = 20;
+        int x1 = -10;
+        int y1 = 40;
+        int x2 = -10;
+        int y2 = 20;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+    @Test
+    public void when3030to2020then70() {
+        double expected = 70.71;
+        int x1 = 30;
+        int y1 = 30;
+        int x2 = -20;
+        int y2 = -20;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
