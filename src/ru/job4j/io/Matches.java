9d3d6169902116cc574f3de65d6a7a9f0e8b141d
Aleksey VV <avbavnn@gmail.com>
2020-10-27 20:17:23 +0300

    2. Scanner и чтение числа из консоли. [#354677]  добавил номера игроков.

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -6,21 +6,26 @@ public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Это игра 11. Смысл игры в следующем. На столе лежат 11 спичек. Два игрока по очереди берут от 1 до 3 спичек. Выигрывает тот, кто забрал последние спички.");
-        System.out.print("Первому игроку приготовиться. ");
+        System.out.println("Первому игроку приготовиться. ");
         int table = 11;
+        boolean player = true;
         while (table > 0) {
-            System.out.println("Сколько спичек вы заберете: ");
+            String name = player ? "player_1" : "player_2";
+            System.out.println(name + " Сколько спичек вы заберете? ");
             int select = Integer.valueOf(input.nextLine());
             if (select < 1 || select > 3) {
                 System.out.println("Пожалуйста, соблюдайте правила.");
                 continue;
             } else {
-                System.out.println("Игрок забрал " + select + " спички.");
+                System.out.println(name + " забрал " + select + " спички.");
                 table = table - select;
                 System.out.println(table + " спичек осталось на столе.");
+                player = !player;
             }
             System.out.println();
         }
-        System.out.println(" Спичек не осталось. Игра окончена. Вы победили!");
+        player = !player;
+        String name = player ? "player_1" : "player_2";
+        System.out.println(" Спичек не осталось. Игра окончена. " + name + " победил.");
     }
 }
